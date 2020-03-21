6cd8d92f5d59614c8c9e1ba6c53d3c1f5ccf4a33
IPpaTsuEr <60258118+IPpaTsuEr@users.noreply.github.com>
2020-03-21 13:00:15 +0800

    Update

---
+++ Code/ExToolKit/Helpers/RotateTransformHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace ExToolKit.Helpers
+{
+    public class RotateTransformHelper
+    {
+        private static void ValueChange(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            var _target = dp as FrameworkElement;
+            if (_target == null) return;
+            
+            var _enable = GetEnableAutoLoop(dp);
+            if (_enable == null) return;
+
+            if (_enable == true) {
+                var _animation = GetAnimation(dp) as AnimationTimeline;
+                if (_animation == null) return;
+                if (!_target.IsLoaded)
+                {
+                    _target.Dispatcher.BeginInvoke(new Action(()=> {
+                        _target.RenderTransform = new RotateTransform();
+                        _target.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, _animation);
+                    }),System.Windows.Threading.DispatcherPriority.Loaded);
+                }
+                else
+                {
+                    _target.RenderTransform = new RotateTransform();
+                    _target.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, _animation);
+                }
+            }
+            else
+            {
+                if(_target.RenderTransform != null)
+                    try
+                    {
+                        _target.RenderTransform = null;
+                       // _target.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, null);
+                    }catch(Exception re) { Console.WriteLine("RotateHelper error :",re.Message); }
+                    
+            }
+        }
+        #region DA
+
+
+
+        public static object GetAnimation(DependencyObject obj)
+        {
+            return (object)obj.GetValue(AnimationProperty);
+        }
+
+        public static void SetAnimation(DependencyObject obj, object value)
+        {
+            obj.SetValue(AnimationProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Animation.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AnimationProperty =
+            DependencyProperty.RegisterAttached("Animation", typeof(object), typeof(RotateTransformHelper), new PropertyMetadata(null, new PropertyChangedCallback(ValueChange)));
+
+
+
+        public static bool? GetEnableAutoLoop(DependencyObject obj)
+        {
+            return (bool?)obj.GetValue(EnableAutoLoopProperty);
+        }
+
+        public static void SetEnableAutoLoop(DependencyObject obj, bool value)
+        {
+            obj.SetValue(EnableAutoLoopProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for EnableAutoLoop.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EnableAutoLoopProperty =
+            DependencyProperty.RegisterAttached("EnableAutoLoop", typeof(bool?), typeof(RotateTransformHelper), new PropertyMetadata(null,new PropertyChangedCallback(ValueChange)));
+
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,61 @@
 	'RequestDll'文件夹下文件为实现GIFImage控件所需要的DLL
 
 
-	此Toolkit仅适用于Elementary系列，Practise系列使用[这个](https://github.com/IPpaTsuEr/WPF-Practise-ToolLib)
+	此Toolkit仅适用于Elementary系列，Practise系列使用![这个](https://github.com/IPpaTsuEr/WPF-Practise-ToolLib)
+	
+## ExWindow 自定义WIndows类
+	
+	FunctionBar [object] 用于放置标题栏控件
+
+## ExPopBar 适用于ExWindow的FunctionBar的控件
+ 继承自HeaderedItemsControl
+	IsSubPoped   [bool] 是否弹出
+	PopBackgroud [SolidColorBrush] 设置背景画刷
+	Command       [ICommand] 命令
+	CommandParameter [object] 命令参数
+	CommandTarget   [IInputElement] 命令目标
+
+## ExPopBarItem 作为ExPopBar的Item的Container
+ 继承自ContentControl
+	Icon [object] 显示icon
+	Command       [ICommand] 命令
+	CommandParameter [object] 命令参数
+	CommandTarget   [IInputElement] 命令目标
+
+## ScrollPanel 用于实现类似于浏览器页面过多时出现的滚动页标签的容器
+ 继承自ItemsControl
+
+## GIFImage GIF控件
+ 实现GIF文件的显示与播放 继承自Image
+	GIFData [string] 指向gif文件的路径
+	Rate    [double] 切换图像的速率，单位：毫秒
+	Index   [int]    图像帧序列
+	Play    [bool]   是否播放GIF动画
+	
+	以下属性仅供读取，设置不会起任何效果
+	Source  用于获取当前显示的GIF帧
+	
+## ArcProgress 弧形进度条
+ 实现圆弧形的进度条 [方案源自互联网]
+	Content    [object] 用于放置内容控件
+	StartAngle [double] 起始角度
+	EndAngle   [double] 终止角度
+	StrokeThickness [double] 弧线宽度
+	Center     [Point]  中心坐标
+	Radius     [double] 圆弧半径
+	
+	以下属性仅供读取，设置不会起任何效果
+	ArcSize    [size] 获取弧大小
+	EndPoint   [point] 获取弧终止点
+	StartPoint [point] 获取弧起始点
+	LargeArc   [bool] 优弧或是劣弧
+	
+## Exbrush 动态笔刷
+ 实现类似windows10 设置界面按钮的边框动态效果 继承自MarkupExtension [方案源自互联网]
+	HighLightColor [Color] 高光颜色，默认为白色
+	NormalColor [Color] 非高光颜色，默认为透明
+	GradientRadius [double] 笔刷高光半径，默认为80
+	BrushOpacity [double] 笔刷透明度，默认值为0.8
+	BrushTransform [Transform] 变换
+	RelativeBrushTransform [Transform] 相对变换
+	GloabalEffect [bool] 当鼠标移出后是否依旧响应鼠标移动事件来动态更新笔刷
